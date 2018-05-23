dfdaa35b33d25e50944d3597c274597d94246654
Luke Bartholomew <lukesbart@gmail.com>
2018-05-23 02:27:36 +0200

    Finish homepage, set manual routing

---
+++ show.js
@@ -0,0 +1,12 @@
+var history = new Vue ({
+  el: '#app2',
+  data: {
+    states: [
+      { text: 'Wessex England', summary: '(900 - 1066) Was the First Unified English Kingdom, Founded by Alfred the Great'},
+      { text: 'Norman England', summary: '(1066 - 1714) Founded by William the Conqueror after the Norman Conquest'},
+      { text: 'Great Britain',  summary: '(1714 - 1800) Created by Act of Parliament by Unification of England and Scotland'},
+      { text: 'United Kingdom', summary: '1800 - Present Created by Act of Parliament by Unification of Great Britain and Ireland'},
+
+    ]
+  }
+});
