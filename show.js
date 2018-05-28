d70e5864a6b3443e8ff9dd5baff2b8dc890647d4
Luke Bartholomew <lukesbart@gmail.com>
2018-05-28 22:29:00 +0200

    Create pages, show page text

--- show.js
+++ show.js
@@ -1,11 +1,21 @@
 var history = new Vue ({
-  el: '#app2',
+  el: '#history',
   data: {
     states: [
-      { text: 'Wessex England', summary: '(900 - 1066) Was the First Unified English Kingdom, Founded by Alfred the Great'},
-      { text: 'Norman England', summary: '(1066 - 1714) Founded by William the Conqueror after the Norman Conquest'},
-      { text: 'Great Britain',  summary: '(1714 - 1800) Created by Act of Parliament by Unification of England and Scotland'},
-      { text: 'United Kingdom', summary: '1800 - Present Created by Act of Parliament by Unification of Great Britain and Ireland'},
+      { text: 'Wessex England',
+      subtitle: `The Kingdom of Wessex was founded by Alfred the Great
+      by the accomplishment of hegemony over the other English Kingdoms.
+      Wessex evolved into the kingdom ruled by Edward the Confessor and
+      conquered by William I in 1066.`},
+
+      { text: 'Norman England', subtitle: `(1066 - 1714) Founded by William
+       the Conqueror after the Norman Conquest`},
+
+      { text: 'Great Britain',  subtitle: `(1714 - 1800) Created by Act of
+      Parliament by Unification of England and Scotland`},
+
+      { text: 'United Kingdom', subtitle: `1800 - Present Created by Act
+      of Parliament by Unification of Great Britain and Ireland`},
 
     ]
   }
