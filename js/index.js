84515e7756e237db89d35c89ea914af75fcde91c
Luke Bartholomew <lukesbart@gmail.com>
2018-05-29 13:31:30 +0200

    Clean up app files

---
+++ js/index.js
@@ -0,0 +1,12 @@
+var app2 = new Vue ({
+  el: '#app2',
+  data: {
+    states: [
+      { link: 'pages/wessex.html', text: 'Wessex England', summary: '(900 - 1066) Was the First Unified English Kingdom, Founded by Alfred the Great'},
+      { link: 'pages/england.html', text: 'Norman England', summary: '(1066 - 1714) Founded by William the Conqueror after the Norman Conquest'},
+      { link: 'pages/britain.html', text: 'Great Britain',  summary: '(1714 - 1800) Created by Act of Parliament by Unification of England and Scotland'},
+      { link: 'pages/united.html', text: 'United Kingdom', summary: '1800 - Present Created by Act of Parliament by Unification of Great Britain and Ireland'},
+
+    ]
+  }
+});
